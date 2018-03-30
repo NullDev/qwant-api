67fc35da0bebee2f25f5adfa9fc424509ae058f6
NullDev (Shadow) <NL-Dev@yandex.com>
2018-03-30 11:25:13 +0200

    Added tests

--- test/test.js
+++ test/test.js
@@ -1,7 +1,22 @@
 "use strict";
 var qwant = require("../lib/qwant-api");
 
-qwant.search("web", { query: "test" }, function(err, data){
+qwant.search("web", { query: "nodejs" }, function(err, data){
     if (err) return console.log(err);
-    console.log(data);
+    console.log("Web Search: \n\n" + JSON.stringify(data, null, 4) + "\n\n----\n");
+});
+
+qwant.search("images", { query: "cat" }, function(err, data){
+    if (err) return console.log(err);
+    console.log("Image Search: \n\n" + JSON.stringify(data, null, 4) + "\n\n----\n");
+});
+
+qwant.search("news", { query: "bitcoin" }, function(err, data){
+    if (err) return console.log(err);
+    console.log("News Search: \n\n" + JSON.stringify(data, null, 4) + "\n\n----\n");
+});
+
+qwant.search("social", { query: "null_ping twitter" }, function(err, data){
+    if (err) return console.log(err);
+    console.log("Social Search: \n\n" + JSON.stringify(data, null, 4) + "\n\n----\n");
 });
